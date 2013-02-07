ae7c07bd7be1ca0d955da0d90298a159c5c3b966
Andrew Bradford <andrew@bradfordembedded.com>
2013-02-06 20:24:54 -0500

    tester: Setup and tear down curl ops in thread

--- tester.c
+++ tester.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define CURL_SIMUL	128
+#define LIST_SIZE	1024
+
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 LIST_HEAD(listhead, entry) head;
@@ -13,29 +16,60 @@ int list_len;
 
 struct entry {
 	char *data;
+	char *hostname;
 	LIST_ENTRY(entry) entries;
 };
 
 void *curl_thread(void *aa)
 {
-	int count;
+	int count, ret, still_left;
+	CURLM *multi_handle;
+	CURL *easy_handle;
+	CURLMsg *msg;
+	struct entry *entry;
+
+	multi_handle = curl_multi_init();
 	for (;;) {
-		count = 0;
+		count = ret = 0;
 		pthread_mutex_lock(&mutex);
 		while (!head.lh_first)
 			pthread_cond_wait(&cond, &mutex);
-		while (head.lh_first && (count < 128)) {
-			/* TODO: Build some curls */
+		while (head.lh_first && (count < CURL_SIMUL)) {
+			entry = head.lh_first;
+			LIST_REMOVE(entry, entries);
+			easy_handle = curl_easy_init();
+			curl_easy_setopt(easy_handle, CURLOPT_VERBOSE, 0);
+			curl_easy_setopt(easy_handle, CURLOPT_NOSIGNAL, 1);
+			curl_easy_setopt(easy_handle, CURLOPT_FAILONERROR, 1);
+			curl_easy_setopt(easy_handle, CURLOPT_NOPROGRESS, 1);
+			curl_easy_setopt(easy_handle, CURLOPT_TIMEOUT, 1);
+			curl_easy_setopt(easy_handle, CURLOPT_POST, 1);
+			curl_easy_setopt(easy_handle, CURLOPT_COPYPOSTFIELDS,
+					 entry->data);
+			curl_easy_setopt(easy_handle, CURLOPT_URL,
+					 entry->hostname);
+			ret = curl_multi_add_handle(multi_handle, easy_handle);
+		}
+		pthread_mutex_unlock(&mutex);
+		/* TODO: Send the curls! */
+		msg = curl_multi_info_read(multi_handle, &still_left);
+		while (msg) {
+			curl_multi_remove_handle(multi_handle,
+						 msg->easy_handle);
+			curl_easy_cleanup(msg->easy_handle);
+			msg = curl_multi_info_read(multi_handle, &still_left);
 		}
 	}
+	curl_multi_cleanup(multi_handle);
 }
 
 int main(void)
 {
 	int ret;
-	struct entry *a;
+	struct entry *entry;
 	pthread_t thread;
 	list_len = 0;
+
 	ret = pthread_create(&thread, NULL, curl_thread, NULL);
 	if (ret)
 		return -1;
