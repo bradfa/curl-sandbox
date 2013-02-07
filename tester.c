43d959d61ac3a7a8a93dd66284089819018c8bd4
Andrew Bradford <andrew@bradfordembedded.com>
2013-02-06 20:11:20 -0500

    tester: Launch a thread, do nothing useful

--- tester.c
+++ tester.c
@@ -1,4 +1,36 @@
+#define MALLOC_CHECK_ 2
+
+#include <pthread.h>
+#include <curl/curl.h>
+#include <sys/queue.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+LIST_HEAD(listhead, entry) head;
+
+struct entry {
+	char *data;
+	LIST_ENTRY(entry) entries;
+};
+
+void *curl_thread(void *aa)
+{
+	for (;;) {
+		sleep(1);
+	}
+}
+
 int main(void)
 {
+	int ret;
+	pthread_t thread;
+	ret = pthread_create(&thread, NULL, curl_thread, NULL);
+	if (ret)
+		return -1;
+	for (;;) {
+		sleep(1);
+	}
 	return 0;
 }
